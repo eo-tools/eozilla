c1c10e1d12ec59a7a066d6d0265d6586d5f531c5
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-10-23 13:00:36 +0200

    First version of appligator works

--- appligator/src/tests/test_version.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2025 by the Eozilla team and contributors
-#  Permissions are hereby granted under the terms of the Apache 2.0 License:
-#  https://opensource.org/license/apache-2-0.
-
-from appligator import __version__
-
-
-def test_version():
-    assert __version__ >= "0.0.0"
