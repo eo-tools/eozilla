563cd5eecef93d198894c29de7ea685da99be588
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-12-05 09:16:40 +0100

    new utility function extend_model()

--- gavicore/src/gavicore/util/model.py
+++ gavicore/src/gavicore/util/model.py
@@ -2,4 +2,49 @@
 #  Permissions are hereby granted under the terms of the Apache 2.0 License:
 #  https://opensource.org/license/apache-2-0.
 
+from typing import TypeVar
 
+
+from pydantic import BaseModel
+from pydantic.fields import FieldInfo
+
+
+T1 = TypeVar("T1", bound=BaseModel)
+T2 = TypeVar("T2", bound=BaseModel)
+
+
+def extend_model(model_cls: T1, extension_cls: T2) -> T1:
+    """
+    A utility function that allows for dynamically adding custom fields
+    to a Pydantic v2 model class.
+    A typical use case is adding vendor-specific extension field
+    to API model classes.
+
+    If a field from `extension_cls` exists in `model_cls`,
+    their field properties are merged.
+
+    If `extension_cls` has no fields, the function is a no-op.
+
+    Args:
+        model_cls: The model class to be enhanced.
+        extension_cls: The model class to that provides additional fields.
+
+    Returns:
+        The `model_cls` instance.
+    """
+    # noinspection PyTypeChecker
+    old_fields: dict[str, FieldInfo] = model_cls.model_fields
+    # noinspection PyTypeChecker
+    new_fields: dict[str, FieldInfo] = dict(extension_cls.model_fields)
+    if not new_fields:
+        return model_cls
+    for name, new_field in new_fields.items():
+        if name in old_fields:
+            old_field = old_fields[name]
+            new_fields[name] = new_field.merge_field_infos(old_field)
+    assert hasattr(model_cls, "__pydantic_fields__")
+    assert isinstance(model_cls.__pydantic_fields__, dict)
+    model_cls.__pydantic_fields__.update(new_fields)
+    model_cls.__pydantic_complete__ = False
+    model_cls.model_rebuild()
+    return model_cls
--- gavicore/tests/util/test_model.py
+++ gavicore/tests/util/test_model.py
@@ -4,43 +4,46 @@
 
 from typing import Annotated, Optional
 
-import pytest
-
 from pydantic import BaseModel, Field
-from pydantic.fields import FieldInfo
+
+from gavicore.util.model import extend_model
 
 
-class MyModel(BaseModel):
-    level: Annotated[int, Field(ge=0)] = 0
+def test_extend_model():
+    class MyModel(BaseModel):
+        level: Annotated[int, Field(ge=0)] = 0
 
+    class MyExtension(BaseModel):
+        mode: Optional[str] = None
 
-class MyModelExtension(BaseModel):
-    mode: Optional[str] = None
-
-
-def test_pydantic_allows_for_dyn_fields():
     model = MyModel(level=3)
     assert model.model_dump(mode="json") == {"level": 3}
 
     # noinspection PyTypeChecker
-    old_fields: dict[str, FieldInfo] = MyModel.model_fields
-    # noinspection PyTypeChecker
-    new_fields: dict[str, FieldInfo] = MyModelExtension.model_fields
-    for name,new_field in new_fields.items():
-        if name in old_fields:
-            old_field = old_fields[name]
-            new_fields[name] = new_field.merge_field_infos(old_field)
-    if new_fields:
-        if hasattr(MyModel, '__pydantic_fields__'):
-            MyModel.__pydantic_fields__.update(new_fields)
-        else:
-            MyModel.__pydantic_fields__ = dict(new_fields)
-        MyModel.__pydantic_complete__ = False
-        MyModel.model_rebuild()
+    model_cls = extend_model(MyModel, MyExtension)
+    assert model_cls is MyModel
 
     model = MyModel(level=5)
-    assert model.model_dump(mode="json") == {"level": 5, 'mode': None}
+    assert model.model_dump(mode="json") == {"level": 5, "mode": None}
 
+    # noinspection PyArgumentList
     model = MyModel(level=7, mode="advanced")
     assert model.model_dump(mode="json") == {"level": 7, "mode": "advanced"}
 
+
+def test_extend_model_empty():
+    class MyModel(BaseModel):
+        level: Annotated[int, Field(ge=0)] = 0
+
+    class MyEmptyExtension(BaseModel):
+        pass
+
+    model = MyModel(level=3)
+    assert model.model_dump(mode="json") == {"level": 3}
+
+    # noinspection PyTypeChecker
+    model_cls = extend_model(MyModel, MyEmptyExtension)
+    assert model_cls is MyModel
+
+    model = MyModel(level=5)
+    assert model.model_dump(mode="json") == {"level": 5}
