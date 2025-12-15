9627990ea980d7194b0e0a04f446ad0a002aac4f
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-12-15 15:32:04 +0100

    adding tests

--- cuiman/src/cuiman/gui/jobs_event_bus.py
+++ cuiman/src/cuiman/gui/jobs_event_bus.py
@@ -1,9 +1,14 @@
+#  Copyright (c) 2025 by the Eozilla team and contributors
+#  Permissions are hereby granted under the terms of the Apache 2.0 License:
+#  https://opensource.org/license/apache-2-0.
+
 import warnings
 from typing import Any, Callable
 from weakref import WeakSet
 
-from .jobs_observer import JobsObserver
 from gavicore.models import JobInfo, JobList
+
+from .jobs_observer import JobsObserver
 
 
 class JobsEventBus:
---
+++ cuiman/tests/gui/test_jobs_event_bus.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2025 by the Eozilla team and contributors
+#  Permissions are hereby granted under the terms of the Apache 2.0 License:
+#  https://opensource.org/license/apache-2-0.
+
+from cuiman.gui.jobs_event_bus import JobsEventBus
+
+
+def test_with_date_input(self):
+    bus = JobsEventBus()
