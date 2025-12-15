989a2fb99082281b2013c02c4abcad82bd15755a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2025-12-15 15:07:54 +0100

    New jobs event bus

---
+++ cuiman/src/cuiman/gui/event_bus.py
@@ -0,0 +1,52 @@
+import warnings
+from typing import Any, Callable
+from weakref import WeakSet
+
+from .jobs_observer import JobsObserver
+from gavicore.models import JobInfo, JobList
+
+
+class JobsEventBus:
+    def __init__(self):
+        self._jobs: dict[str, JobInfo] = {}
+        self._jobs_observers: WeakSet[JobsObserver] = WeakSet()
+
+    def register(self, jobs_observer: JobsObserver):
+        self._jobs_observers.add(jobs_observer)
+
+    def dispatch_job_list(self, job_list: JobList):
+        old_jobs = self._jobs
+        new_jobs = {job.jobID: job for job in job_list.jobs}
+
+        added_jobs = [job for job_id, job in new_jobs.items() if job_id not in old_jobs]
+        changed_jobs = [
+            job
+            for job_id, job in new_jobs.items()
+            if job_id in old_jobs and job != old_jobs[job_id]
+        ]
+        removed_jobs = [
+            job for job_id, job in old_jobs.items() if job_id not in new_jobs
+        ]
+
+        if added_jobs or changed_jobs or removed_jobs:
+            for job in added_jobs:
+                self._emit("job_added", job)
+            for job in changed_jobs:
+                self._emit("job_changed", job)
+            for job in removed_jobs:
+                self._emit("job_removed", job)
+            self._emit("job_list_changed", job_list)
+            self._jobs = new_jobs
+
+    def _emit(self, event_type: str, event_arg: Any):
+        for jobs_observer in self._jobs_observers:
+            try:
+                method: Callable[[Any], None] = getattr(
+                    jobs_observer, f"on_{event_type}"
+                )
+                method(event_arg)
+            except ReferenceError:
+                # handle case where reference is already gone, this is ok
+                pass
+            except BaseException as e:
+                warnings.warn(f"Error emitting event of type {event_type!r}: {e}")
