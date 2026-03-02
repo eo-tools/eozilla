ba6c1ddd76f9c18a9f26212f1f92e6be32876397
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-03-02 14:34:23 +0100

    Update copyright notice

--- eozilla-airflow/dags/sleep_a_while.py
+++ eozilla-airflow/dags/sleep_a_while.py
@@ -1,3 +1,7 @@
+#  Copyright (c) 2026 by the Eozilla team and contributors
+#  Permissions are hereby granted under the terms of the Apache 2.0 License:
+#  https://opensource.org/license/apache-2-0.
+
 # WARNING - THIS IS GENERATED CODE
 #   Generator: Eozilla Appligator v0.0.5.dev0
 #        Date: 2025-10-23T11:47:13.128497
@@ -8,21 +12,21 @@ from wraptile.services.local.testing import sleep_a_while
 
 
 @dag(
-    'sleep_a_while',
-    dag_display_name='Sleep Processor',
-    description='Sleeps for `duration` seconds. Fails on purpose if `fail` is `True`. Returns the effective amount of sleep in seconds.',
+    "sleep_a_while",
+    dag_display_name="Sleep Processor",
+    description="Sleeps for `duration` seconds. Fails on purpose if `fail` is `True`. Returns the effective amount of sleep in seconds.",
     params={
-        'duration': Param(default=10.0, type='number', title='Duration'),
-        'fail': Param(default=False, type='boolean', title='Fail'),
+        "duration": Param(default=10.0, type="number", title="Duration"),
+        "fail": Param(default=False, type="boolean", title="Fail"),
     },
     is_paused_upon_creation=False,
 )
 def sleep_a_while_dag():
-
     @task(multiple_outputs=False)
     def sleep_a_while_task(params):
         return sleep_a_while(**params)
 
     task_instance = sleep_a_while_task()  # noqa: F841
+
 
 sleep_a_while_dag()
--- procodile/tests/test_registry.py
+++ procodile/tests/test_registry.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2025 by the Eozilla team and contributors
+#  Copyright (c) 2025-2026 by the Eozilla team and contributors
 #  Permissions are hereby granted under the terms of the Apache 2.0 License:
 #  https://opensource.org/license/apache-2-0.
 import unittest
