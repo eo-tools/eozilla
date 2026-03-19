789120fe7bef2e1e01b699f5b43b8981b21950d7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-03-19 07:51:56 +0100

    factory impl

--- cuiman/src/cuiman/api/ui/field.py
+++ cuiman/src/cuiman/api/ui/field.py
@@ -14,9 +14,10 @@ VT = TypeVar("VT")
 
 
 class UIField(Generic[VMT, VT], ABC):
-    @abstractmethod
-    def get_meta(self) -> UIFieldMeta:
-        """Return the field metadata."""
+    """
+    Adapter that allows using a view `V` from some UI component library
+    together with a view model `VMT` of type `ViewModel`.
+    """
 
     @abstractmethod
     def get_view_model(self) -> VMT:
@@ -28,10 +29,13 @@ class UIField(Generic[VMT, VT], ABC):
 
 
 class UIFieldBase(Generic[VMT, VT], UIField[VMT, VT], ABC):
-    def __init__(self, meta: UIFieldMeta):
-        self._meta = meta
+    """Abstract base class for UI fields."""
 
-    def get_meta(self) -> UIFieldMeta:
+    def __init__(self, field_meta: UIFieldMeta):
+        self._meta = field_meta
+
+    @property
+    def meta(self) -> UIFieldMeta:
         return self._meta
 
 
@@ -76,59 +80,120 @@ class UIBuilderContext:
 
 class UIFieldFactory(ABC):
     @abstractmethod
-    def get_score(self, ctx: UIBuilderContext) -> int:
-        """Get the score of this factory for the given builder context."""
+    def get_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score of this factory for the given field metadata."""
 
     @abstractmethod
     def create_field(self, ctx: UIBuilderContext) -> UIField:
         """Create the UI field for the given builder context."""
 
 
+# noinspection PyMethodMayBeStatic,PyUnusedLocal
 class UIFieldFactoryBase(UIFieldFactory, ABC):
-    def get_score(self, ctx: UIBuilderContext) -> int:
+    """
+    A convenient base class for UI field factories that create
+    UI fields based on the schema data type.
+
+    You need to override one or more of the `get_{type}_score()`
+    methods and return a value greater zero.
+    By default, all `get_{type}_score()` return zero.
+
+    Make sure to also implement any `create_{type}_field()` method
+    for which `get_{type}_score()` returns a value greater zero.
+    By default, all `create_{type}_field()` raise a `NotImplementedError`.
+    """
+
+    def get_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field based on its data type."""
+        match field_meta.schema_.type:
+            case DataType.object:
+                return self.get_object_score(field_meta)
+            case DataType.array:
+                return self.get_array_score(field_meta)
+            case DataType.string:
+                return self.get_string_score(field_meta)
+            case DataType.number:
+                return self.get_number_score(field_meta)
+            case DataType.integer:
+                return self.get_integer_score(field_meta)
+            case DataType.boolean:
+                return self.get_boolean_score(field_meta)
+            case _:
+                return self.get_untyped_score(field_meta)
+
+    def get_object_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "object"."""
+        return 0
+
+    def get_array_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "array"."""
+        return 0
+
+    def get_string_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "string"."""
+        return 0
+
+    def get_number_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "number"."""
+        return 0
+
+    def get_integer_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "integer"."""
+        return 0
+
+    def get_boolean_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field of type "boolean"."""
+        return 0
+
+    def get_untyped_score(self, field_meta: UIFieldMeta) -> int:
+        """Get the score for a field that has no explicit type."""
+        return 0
+
+    def create_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field based on its data type."""
         schema_type = ctx.schema.type
-        if schema_type == DataType.object:
-            return self.get_object_score(ctx)
-        elif schema_type == DataType.array:
-            return self.get_array_score(ctx)
-        elif schema_type is not None:
-            return self.get_primitive_score(ctx)
-        else:
-            return self.get_untyped_score(ctx)
+        match schema_type:
+            case DataType.object:
+                return self.create_object_field(ctx)
+            case DataType.array:
+                return self.create_array_field(ctx)
+            case DataType.string:
+                return self.create_string_field(ctx)
+            case DataType.number:
+                return self.create_number_field(ctx)
+            case DataType.integer:
+                return self.create_integer_field(ctx)
+            case DataType.boolean:
+                return self.create_boolean_field(ctx)
+            case _:
+                return self.create_untyped_field(ctx)
 
-    def get_object_score(self, ctx: UIBuilderContext) -> int:
-        return 0
-
-    def get_array_score(self, ctx: UIBuilderContext) -> int:
-        return 0
-
-    def get_primitive_score(self, ctx: UIBuilderContext) -> int:
-        return 0
-
-    def get_untyped_score(self, ctx: UIBuilderContext) -> int:
-        return 0
-
-    def create_field(self, ctx: UIBuilderContext) -> UIField[VMT, VT]:
-        schema_type = ctx.schema.type
-        if schema_type == DataType.object:
-            return self.create_object_field(ctx)
-        elif schema_type == DataType.array:
-            return self.create_array_field(ctx)
-        elif schema_type is not None:
-            return self.create_primitive_field(ctx)
-        else:
-            return self.create_untyped_field(ctx)
-
-    def create_object_field(self, ctx: UIBuilderContext) -> UIField[VMT, VT]:
+    def create_object_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "object"."""
         raise NotImplementedError
 
-    def create_array_field(self, ctx: UIBuilderContext) -> UIField[VMT, VT]:
+    def create_array_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "array"."""
         raise NotImplementedError
 
-    def create_primitive_field(self, ctx: UIBuilderContext) -> UIField[VMT, VT]:
+    def create_string_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "string"."""
         raise NotImplementedError
 
-    def create_untyped_field(self, ctx: UIBuilderContext) -> UIField[VMT, VT]:
+    def create_number_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "number"."""
+        raise NotImplementedError
+
+    def create_integer_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "integer"."""
+        raise NotImplementedError
+
+    def create_boolean_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value of type "boolean"."""
+        raise NotImplementedError
+
+    def create_untyped_field(self, ctx: UIBuilderContext) -> UIField:
+        """Create a UI field for a value with no explicit type given."""
         raise NotImplementedError
 
 
@@ -139,25 +204,25 @@ class UIFieldBuilder:
     def register_factory(self, factory: UIFieldFactory):
         self._factories.append(factory)
 
-    def find_factory(self, ctx: UIBuilderContext) -> UIFieldFactory | None:
+    def find_factory(self, field_meta: UIFieldMeta) -> UIFieldFactory | None:
         max_score = 0
         best_factory: UIFieldFactory | None = None
         for f in self._factories:
-            s = f.get_score(ctx)
+            s = f.get_score(field_meta)
             if s > max_score:
                 max_score = s
                 best_factory = f
         return best_factory
 
+    def create_ctx(
+        self, field_meta: UIFieldMeta, parent_ctx: UIBuilderContext | None = None
+    ) -> UIBuilderContext:
+        return UIBuilderContext(self, field_meta, parent_ctx=parent_ctx)
+
     def create_field(self, ctx: UIBuilderContext) -> UIField:
-        factory = self.find_factory(ctx)
+        factory = self.find_factory(ctx.field_meta)
         if factory is None:
             raise ValueError(
                 f"No factory found for creating a UI for field {'.'.join(ctx.path)!r}"
             )
         return factory.create_field(ctx)
-
-    def create_ctx(
-        self, field_meta: UIFieldMeta, parent_ctx: UIBuilderContext | None = None
-    ) -> UIBuilderContext:
-        return UIBuilderContext(self, field_meta, parent_ctx=parent_ctx)
--- cuiman/tests/api/ui/test_field.py
+++ cuiman/tests/api/ui/test_field.py
@@ -18,9 +18,9 @@ from cuiman.api.ui.vm import (
     ObjectViewModel,
     PrimitiveViewModel,
 )
-from gavicore.models import DataType, Schema
+from gavicore.models import Schema
 
-# -----------
+# --- A component library --------
 
 
 class Component:
@@ -29,22 +29,22 @@ class Component:
         self.kwargs = kwargs
 
 
-class Panel(Component):
+class PanelComponent(Component):
     pass
 
 
-class ListEditor(Component):
+class ListEditorComponent(Component):
     pass
 
 
-class TextField(Component):
+class TextComponent(Component):
     pass
 
 
-# -----------
+# --- UI field adapters --------
 
 
-class MyObjectField(UIFieldBase[ObjectViewModel, Panel]):
+class MyObjectField(UIFieldBase[ObjectViewModel, PanelComponent]):
     def __init__(
         self,
         field_meta: UIFieldMeta,
@@ -56,42 +56,42 @@ class MyObjectField(UIFieldBase[ObjectViewModel, Panel]):
         view_models = [f.get_view_model() for f in child_ui_fields]
         # TODO: we should pass the dict of view_models to ObjectViewModel
         self.vm = ObjectViewModel(field_meta, None, value)
-        self.panel = Panel(views=views)
+        self.panel = PanelComponent(views=views)
 
     def get_view_model(self) -> ObjectViewModel:
         return self.vm
 
-    def get_view(self) -> Panel:
+    def get_view(self) -> PanelComponent:
         return self.panel
 
 
-class MyArrayField(UIFieldBase[ArrayViewModel, ListEditor]):
+class MyArrayField(UIFieldBase[ArrayViewModel, ListEditorComponent]):
     def __init__(self, field_meta: UIFieldMeta):
         super().__init__(field_meta)
         self.vm = ArrayViewModel(field_meta, None, None)
-        self.view = ListEditor()
+        self.view = ListEditorComponent(field_meta, None, None)
 
     def get_view_model(self) -> ArrayViewModel:
         return self.vm
 
-    def get_view(self) -> ListEditor:
+    def get_view(self) -> ListEditorComponent:
         return self.view
 
 
-class MyTextField(UIFieldBase[PrimitiveViewModel[str], TextField]):
+class MyTextField(UIFieldBase[PrimitiveViewModel[str], TextComponent]):
     def __init__(self, field_meta: UIFieldMeta):
         super().__init__(field_meta)
         self.node = PrimitiveViewModel(field_meta, None, None)
-        self.text_field = TextField()
+        self.text_field = TextComponent()
 
     def get_view_model(self) -> PrimitiveViewModel[str]:
         return self.node
 
-    def get_view(self) -> TextField:
+    def get_view(self) -> TextComponent:
         return self.text_field
 
 
-# -----------
+# --- Factories for the field adapters --------
 
 
 class MyObjectFactory(UIFieldFactoryBase):
@@ -114,14 +114,14 @@ class MyArrayFactory(UIFieldFactoryBase):
 
 
 class MyTextFactory(UIFieldFactoryBase):
-    def get_primitive_score(self, ctx: UIBuilderContext) -> int:
-        return 1 if ctx.schema.type == DataType.string else 0
+    def get_string_score(self, field_meta: UIFieldMeta) -> int:
+        return 1
 
-    def create_field(self, ctx: UIBuilderContext) -> UIField:
+    def create_string_field(self, ctx: UIBuilderContext) -> UIField:
         return MyTextField(ctx.field_meta)
 
 
-# -----------
+# --- UI field builder usage --------
 
 
 class UIFieldBuilderTest(TestCase):
@@ -145,6 +145,6 @@ class UIFieldBuilderTest(TestCase):
             ),
         )
 
-        ctx = b.create_ctx(field_meta)
+        ctx = b.create_ctx(field_meta, value={"dataset": []})
         field = b.create_field(ctx)
         self.assertIsInstance(field, MyObjectField)
